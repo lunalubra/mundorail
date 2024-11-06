a1b82b80ffa2c6822e0a4639e2afe7a188435571
lunalubra <lucas2002luna@gmail.com>
2024-11-06 17:03:32 +0100

    only redirect once to mexico

--- src/lib/redirect.ts
+++ src/lib/redirect.ts
@@ -2,11 +2,11 @@ import { getCountry } from "./getCountry";
 
 export const redirect = () => {
   if (typeof window !== "undefined") {
+    const domain =
+      location?.host?.split(".")[location?.host?.split(".").length - 1];
+    const isGlobalPage = domain === "com";
+    const isMexicoPage = domain === "mx";
     try {
-      const domain =
-        location?.host?.split(".")[location?.host?.split(".").length - 1];
-      const isGlobalPage = domain === "com";
-      const isMexicoPage = domain === "mx";
       const country = getCountry();
       const isInUSA = country === "United States of America";
       const isInMexico = country === "Mexico";
@@ -18,7 +18,9 @@ export const redirect = () => {
       }
     } catch (error) {
       console.log(error);
-      window.location.host = "www.mundorail.mx" + window.location.pathname;
+      if (!isMexicoPage) {
+        window.location.host = "www.mundorail.mx" + window.location.pathname;
+      }
     }
   }
 };
